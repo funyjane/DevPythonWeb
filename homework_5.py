971c5326478004cef3d0dcf603c8606931a79052
Danila Stasiuk <danila.stasiuk@gmail.com>
2020-09-23 14:58:54 +0300

    looks like decorator is now working but not the tri_recursion func

--- homework_5.py
+++ homework_5.py
@@ -1,25 +1,15 @@
 creds = {'dan' : 'test321', 'misha' : 'test123'}
 
-
 def enter_creds(func):
-    user_name = input("Please enter your username: ")
-    user_pwd = input("Please enter your password: ")
+    
 
-    def wrapper():
+    def wrapper(*args):
+        user_name = input("Please enter your username: ")
+        user_pwd = input("Please enter your password: ")
         if creds.get(user_name) == user_pwd:
-            print('ok')
+            print('correct')
         else:
             print('user name or password incorrect')
-
-    # проверка
-        if user_name in creds:
-            if creds[user_name] == user_pwd:
-                print('correct')
-            else:
-                print('wrong password')
-        else:
-            print('wrong user name')
-
     # если не сработает
         try:
             if creds[user_name] == user_pwd:
@@ -28,6 +18,7 @@ def enter_creds(func):
                 print('wrong password')
         except:
             print('wrong user name')
+    return wrapper
 
 
 @enter_creds
