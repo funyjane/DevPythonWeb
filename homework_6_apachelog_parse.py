f614f5e2030110771f7b018ea8ff33ec7a327150
Danila Stasiuk <danila.stasiuk@gmail.com>
2020-09-27 20:50:27 +0300

    Final commit apache log parser

--- homework_6_apachelog_parse.py
+++ homework_6_apachelog_parse.py
@@ -8,7 +8,15 @@ from collections import Counter
 
 ip_set = set()
 ip_list = []
+browsers = {'Firefox':0,
+'Chrome': 0,
+'Edge': 0,
+'Explorer': 0,
+'Opera': 0,
+'Safari': 0}
 
+
+#1. Сколько было всего запросов
 def reader(file_name):
 
    with open (file_name) as file: 
@@ -20,6 +28,7 @@ def reader(file_name):
          ip_list.append(ips.group(0))
    print('Сколько было всего запросов: ' + str(len(ip_list)))
 
+#2. Сколько уникальных ip адресов обратилось
 def reader_1(file_name):
 
    with open (file_name) as file: 
@@ -31,17 +40,21 @@ def reader_1(file_name):
          ip_set.add(ips.group(0))
    print('Сколько уникальных ip адресов обратилось: ' + str(len(ip_set)))
 
-
-def reader_1(file_name):
+#Какие браузеры обращались. Для простоты берем запись почти в конце строки (Firefox/34.0)
+def browser_reader(file_name):
 
    with open (file_name) as file: 
       
-      regxp_ips = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
-      
       for line in file:
-         ips = re.search(regxp_ips, line)
-         ip_set.add(ips.group(0))
-   print('Сколько уникальных ip адресов обратилось: ' + str(len(ip_set)))
+         user_request = line[-40:]
+         for keys in browsers.keys():
+            #4. Сколько запросов от каждого такого браузера ''' 
+                if keys in user_request:
+                    browsers[keys] += 1
+      print('Количество запросов от браузеров:')
+      for keys, values in browsers.items():
+            print(keys, '-', values)
+   
 
 if __name__ == "__main__":
-   reader('access.log')
+   browser_reader('access.log')
