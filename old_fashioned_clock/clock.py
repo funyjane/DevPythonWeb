8849749c20eeb5bb9e0f32af1d4ea12e90ad2b0b
Danila Stasiuk <danila.stasiuk@gmail.com>
2020-10-01 19:22:38 +0300

    s

---
+++ old_fashioned_clock/clock.py
@@ -0,0 +1,23 @@
+def get_current_time():
+    # get and return current time some how
+
+    pass
+    # return time
+def print_digits(current_time):
+    pass
+
+def clear_screen():
+    pass
+
+def sleep_for_a_while(period):
+    pass
+
+if __name__ == "__main__":
+    while True:
+        current_time = get_current_time()
+        print_digits(current_time)
+        clear_screen()
+        sleep_for_a_while(0.3)
+
+
+
