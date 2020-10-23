50698b310740eab7c0e2c3e09768685255a3f085
Danila Stasiuk <danila.stasiuk@gmail.com>
2020-10-23 17:39:06 +0300

    adjustd dotenv path

--- bookstore/src/proj/settings.py
+++ bookstore/src/proj/settings.py
@@ -16,6 +16,9 @@ from . import local_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
 
 
 # Quick-start development settings - unsuitable for production
