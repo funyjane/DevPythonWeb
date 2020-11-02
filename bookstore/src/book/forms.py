97e96547dfdf47ec17c73d5a6e7fda014e9bc14e
Danila Stasiuk <danila.stasiuk@gmail.com>
2020-11-02 18:00:13 +0200

    fixed book create func

--- bookstore/src/book/forms.py
+++ bookstore/src/book/forms.py
@@ -6,9 +6,9 @@ from . import models
 class CreateBookForm(forms.ModelForm):
     class Meta:
         model = models.Book
-        fields = '__all__'
+        fields = ('__all__')
 
 class UpdateBookForm(forms.ModelForm):
     class Meta:
         model = models.Book
-        fields = '__all__'
+        fields = ('__all__')
